47dec692f41e61640fa14664ed40754bc4a2a0b5
Trang Nguyen <pneppy@gmail.com>
2021-06-08 00:06:03 +0700

    minor fix

--- src/chart-files/boxplot_walc - grade/script.js
+++ src/chart-files/boxplot_walc - grade/script.js
@@ -1,27 +1,28 @@
-var x = new Array(1044)
-var Grade = new Array(1044)
-d3.csv("https://raw.githubusercontent.com/MrCat-2510/Testing/main/Visual_Data/Book4.csv", function(data) {
+var x = new Array(1044);
+var Grade = new Array(1044);
+d3.csv(
+    "https://raw.githubusercontent.com/MrCat-2510/Testing/main/Visual_Data/Book4.csv",
+    function (data) {
+        data.forEach(function (d, i) {
+            x[i] = d.Walc;
+            Grade[i] = d.Result;
+        });
+        var trace = {
+            y: Grade,
+            x: x,
+            marker: { color: "#69b3a2" },
+            type: "box",
+        };
+        var data = [trace];
 
-    data.forEach(function(d,i){
-      x[i] = d.Walc
-      Grade[i] = d.Result
-    })
-    var trace = {
-      y: Grade,
-      x: x,
-      marker: {color: '#69b3a2'},
-      type: 'box'
-    };
-    var data = [trace];
+        var layout = {
+            yaxis: {
+                title: "Grade",
+                zeroline: true,
+            },
+            boxmode: "group",
+        };
 
-    var layout = {
-      yaxis: {
-        title: 'Grade',
-        zeroline: true
-      },
-      boxmode: 'group'
-    };
-
-    Plotly.newPlot('myDiv', data, layout);
-})
-
+        Plotly.newPlot("myDiv", data, layout);
+    }
+);
